db19802a21343a508e91b50732b901840b89a7c2
OganKuba <kubog123@gmail.com>
2024-02-27 23:52:52 +0100

    ogarnięcie chlewu w kodzie

--- main.py
+++ main.py
@@ -1,83 +1,58 @@
-import time
 import csv
 import json
 import requests
 from bs4 import BeautifulSoup
-from itertools import chain
-import re
 
-SITE_URL = f"https://www.unb.com.bd/api/tag-news?tag_id=54&item="
+SITE_URL = "https://www.unb.com.bd/api/tag-news?tag_id=54&item="
 NUMBER_OF_PAGES = 1
 
 
-def extract_urls(soup):
+def get_json_data(url):
+    response = requests.get(url)
+    return json.loads(response.content)
+
+
+def extract_urls(json_data):
+    soup = BeautifulSoup(json_data["html"], "html.parser")
     links = soup.find_all('a')
-    urls = [link.get('href') for link in links if link.get('href') is not None]
+    urls = set(link.get('href').replace("\\", "") for link in links if link.get('href'))
     return urls
 
 
-def HandleLinkFromList(link):
-    link_test = link.replace('"', "")
-    page = requests.get(link_test)
-    soup = BeautifulSoup(page.content, "html.parser")
-    data = soup.find_all('ul',class_='post-meta hidden-xs')
-    soup = BeautifulSoup(str(data),"html.parser")
-    data = soup.find_all('li',class_='news-section-bar')
-    temp_soup = BeautifulSoup(str(data),"html.parser")
+def process_link(link):
+    response = requests.get(link)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    data = temp_soup.find_all('li')
-    place = data[0].text #potem
-    numberOfDates=0
-    DateTab=[]
-    for html in data:
-        rekord=html.find_all('span',class_='icon qb-clock')
-        if len(rekord) > 0:
-            DateTab.append(html.text.replace("\n","").replace("'",""))
-            numberOfDates+=1
-    place =""
-    for html in data:
-        rekord=html.find_all('span',class_='icon fa fa-map-marker')
-        if len(rekord) > 0:
-            place = html.text
+    date_data = soup.find_all('li', class_='news-section-bar')
+    dates = [item.text.replace("\n", "").replace("'", "") for item in date_data if 'qb-clock' in str(item)]
+    place = [item.text for item in date_data if 'fa-map-marker' in str(item)][0] if date_data else ""
 
-    htmlText=page.content
-    soup = BeautifulSoup(page.content, "html.parser")
-    text_temp = soup.find_all('div', class_='text')
-    soup = BeautifulSoup(str(text_temp[0]), "html.parser")
-    for data in soup(['style', 'script']):
-        # Remove tags
-        data.decompose()
-
-    # return data by retrieving the tag content
-    articleText = ' '.join(soup.stripped_strings)
-    print(articleText)
-    if len(DateTab) == 1:
-        return (DateTab[0],"",place,htmlText,articleText)
+    htmlText = response.content.decode('utf-8').replace('\n', '')
+    article_soup = BeautifulSoup(htmlText, "html.parser")
+    text_div = article_soup.find('div', class_='text')
+    if text_div:
+        for tag in text_div(['style', 'script']):
+            tag.decompose()
+        articleText = ' '.join(text_div.stripped_strings)
     else:
-        return (DateTab[0],DateTab[1],place,htmlText,articleText)
+        articleText = ""
+
+    return (dates[0], dates[1] if len(dates) > 1 else "", place, htmlText, articleText)
+
+
+def save_to_csv(data, filepath):
+    with open(filepath, 'w', newline='', encoding='utf-8') as out:
+        csv_out = csv.writer(out, delimiter=';')
+        csv_out.writerow(('Publish', 'Update', 'Place', 'HTML_TEXT', 'RAW_TEXT'))
+        csv_out.writerows(data)
+
 
 if __name__ == '__main__':
-    final_links = []
+    all_data = []
     for index in range(1, NUMBER_OF_PAGES + 1):
-        page = requests.get(f"{SITE_URL}{index}")
-        json_page = json.loads(page.content)
-
-        while len(json_page["html"]) < 1:
-            page = requests.get(f"{SITE_URL}{index}")
-            json_page = json.loads(page.content)
-        soup = BeautifulSoup(page.content, "html.parser")
-        links = extract_urls(soup)
-        links = list(set(links))
-        links = [w.replace("\\", "") for w in links]
-        final_links += links
-        print(final_links[0])
-    # mamy final_links teraz chcemy przez nie przelecieć
-    krotka = HandleLinkFromList(link=final_links[0])
-
-    print(krotka)
-    with open('C:/Users/bezi1/OneDrive/Dokumenty/output.csv', 'w') as out:
-        csv_out = csv.writer(out)
-        csv_out.writerow(('Publish', 'Update','HTML_TEXT','RAW_TEXT'))
-        csv_out.writerow(krotka)
-
-    exit()
+        json_data = get_json_data(f"{SITE_URL}{index}")
+        urls = extract_urls(json_data)
+        for link in urls:
+            link_data = process_link(link)
+            all_data.append(link_data)
+    save_to_csv(all_data[0], 'output.csv')
